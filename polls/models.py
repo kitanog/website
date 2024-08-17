bb51ecbaad4526c8ec916bd7360308adf75fecda
Kitan Ogunkoya <kiitangnk@gmail.com>
2024-08-17 19:28:35 -0400

    Update models.py
    update

--- polls/models.py
+++ polls/models.py
@@ -41,4 +41,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     #adding a string to the choice ID
     def __str__(self):
-        return self.choice_text
+        return slf.choice_text
